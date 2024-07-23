3db45358dcae90946868633d909cd26dae0c99d0
Thulasimani <157271044+thulasimani003@users.noreply.github.com>
2024-07-23 16:40:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector('.accept').addEventListener('click', function() {
+    alert('You clicked Accept!');
+});
+
+document.querySelector('.decline').addEventListener('click', function() {
+    alert('You clicked Decline!');
+});
